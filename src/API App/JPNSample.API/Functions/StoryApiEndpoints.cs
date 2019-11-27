d56060ee1f5a2b1ab9ffa37cf35ea2d320d494bd
jessenich91 <jessenich@live.com>
2019-11-27 11:09:28 -0500

    Added extra request checks and conditional results for client debugging purposes

--- src/API App/JPNSample.API/Functions/StoryApiEndpoints.cs
+++ src/API App/JPNSample.API/Functions/StoryApiEndpoints.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Web.Http;
 using JPNSample.API.Core;
 using JPNSample.API.Core.Cache;
 using JPNSample.API.Core.Integration.HackerNews;
@@ -33,33 +34,40 @@ namespace JPNSample.API.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "types")] HttpRequest req,
             ILogger logger)
         {
-            var idKeys = new List<string>() {
-                CacheKeys.NewStoriesCacheKey,
-                CacheKeys.TopStoriesCacheKey,
-                CacheKeys.BestStoriesCacheKey
-            };
-
-            dynamic getStoryIdGroup(IEnumerable<CacheItem<IEnumerable<int>>> caches, string storyType)
+            try
             {
-                var cache = caches.FirstOrDefault(x => x.ExtendedProperties[CacheExtendedPropertiesConstants.StoryTypeKey] == storyType);
-                return new {
-                    key = cache.ExtendedProperties[CacheExtendedPropertiesConstants.StoryTypeKey],
-                    count = cache.Item.Count(),
-                    items = cache.Item
+                var idKeys = new List<string>() {
+                    CacheKeys.NewStoriesCacheKey,
+                    CacheKeys.TopStoriesCacheKey,
+                    CacheKeys.BestStoriesCacheKey
                 };
+
+                dynamic getStoryIdGroup(IEnumerable<CacheItem<IEnumerable<int>>> caches, string storyType)
+                {
+                    var cache = caches.FirstOrDefault(x => x.ExtendedProperties[CacheExtendedPropertiesConstants.StoryTypeKey] == storyType);
+                    return new {
+                        key = cache.ExtendedProperties[CacheExtendedPropertiesConstants.StoryTypeKey],
+                        count = cache.Item.Count(),
+                        items = cache.Item
+                    };
+                }
+
+                var cacheResults = await _cache.GetManyAsync<IEnumerable<int>>(idKeys);
+                var topResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.TopStoryTypeValue);
+                var newResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.NewStoryTypeValue);
+                var bestResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.BestStoryTypeValue);
+
+                this.AddCorsHeader(req);
+                return new OkObjectResult(new {
+                    topStories = topResults,
+                    newStories = newResults,
+                    bestStories = bestResults
+                });
             }
-
-            var cacheResults = await _cache.GetManyAsync<IEnumerable<int>>(idKeys);
-            var topResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.TopStoryTypeValue);
-            var newResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.NewStoryTypeValue);
-            var bestResults = getStoryIdGroup(cacheResults, CacheExtendedPropertiesConstants.BestStoryTypeValue);
-
-            this.AddCorsHeader(req);
-            return new OkObjectResult(new {
-                top = topResults,
-                @new = newResults,
-                best = bestResults
-            });
+            catch (Exception ex)
+            {
+                return new NotFoundObjectResult(ex.ToString());
+            }
         }
 
         [FunctionName("GetStories")]
@@ -67,24 +75,35 @@ namespace JPNSample.API.Functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "stories")] HttpRequest req,
             ILogger logger)
         {
-            var take = int.Parse(req.Query["take"].FirstOrDefault() ?? "10");
-            var page = int.Parse(req.Query["page"].FirstOrDefault() ?? "1");
+            try
+            {
+                var take = int.Parse(req.Query["take"].FirstOrDefault() ?? "10");
+                var page = int.Parse(req.Query["page"].FirstOrDefault() ?? "1");
 
-            var ids = JsonConvert.DeserializeObject<IEnumerable<int>>(await req.ReadAsStringAsync());
-            var keys = ids.Select(id => string.Format(CacheKeys.StoryContentCacheKeyFormat, id.ToString()));
+                var requestBody = await req.ReadAsStringAsync();
+                if (string.IsNullOrWhiteSpace(requestBody))
+                    return new StatusCodeResult(400);
 
-            var stories = await _cache.GetManyAsync<HackerNewsStoriesResponseModel>(keys);
+                var ids = JsonConvert.DeserializeObject<IEnumerable<int>>(requestBody);
+                if (ids == null || ids.Count() == 0)
+                    return new NoContentResult();
 
-            if (stories.Count() <= ((page - 1) * take))
-                page = 1;
+                var keys = ids.Select(id => string.Format(CacheKeys.StoryContentCacheKeyFormat, id.ToString()));
 
-            var storiesResponse = stories
+                var stories = await _cache.GetManyAsync<HackerNewsStoriesResponseModel>(keys);
+
+                if (stories.Count() <= ((page - 1) * take))
+                    page = 1;
+
+                var storiesResponse = stories
                 .Select(cache => cache.Item)
                 .Where(story => story.Url != null)
                 .OrderByDescending(story => story.Id)
                 .Skip((page - 1) * take)
                 .Take(take)
                 .Select(story => new {
+                    id = story.Id,
+                    type = story.Type,
                     author = story.By,
                     title = story.Title,
                     url = story.Url,
@@ -92,8 +111,13 @@ namespace JPNSample.API.Functions
                     createdAt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(story.Time),
                 });
 
-            this.AddCorsHeader(req);
-            return new OkObjectResult(storiesResponse);
+                this.AddCorsHeader(req);
+                return new OkObjectResult(storiesResponse);
+            }
+            catch (Exception ex)
+            {
+                return new NotFoundObjectResult(ex.ToString());
+            }
         }
 
         private bool AddCorsHeader(HttpRequest request)
